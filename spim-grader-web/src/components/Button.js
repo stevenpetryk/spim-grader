7ccd862a350acff2afb7c6fcf0fbb9a9d21a9e7c
Steven Petryk <me@stevenpetryk.com>
2017-09-16 12:40:00 -0400

    wip

--- spim-grader-web/src/components/Button.js
+++ spim-grader-web/src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import cx from 'classnames'
 
-import './Button.css'
+import './Button.scss'
 
 export default ({ children, className, disabled, onClick = () => {}, ...rest }) => (
   <button
